30c47bc3abeecb7f30d46e01089b2b94cd5e1901
nam <namish@tuta.io>
2024-05-26 11:40:22 +0530

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import remarkMath from 'remark-math';
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: 'https://nam.is-a.dev',
+  site: 'https://chadcat7.github.io',
   base: '/',
   trailingSlash: "never",
   integrations: [tailwind(), mdx({
