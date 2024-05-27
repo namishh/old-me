4e59e5c5a106b6884b8dfef545de091d8b9357ef
nam <namish@tuta.io>
2024-05-28 01:20:33 +0530

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,25 +5,33 @@ import remarkMdxCodeMeta from 'remark-mdx-code-meta';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 import react from "@astrojs/react";
-
+import astroExpressiveCode from "astro-expressive-code";
+import { pluginFrames } from 'astro-expressive-code';
+import { pluginTextMarkers } from 'astro-expressive-code';
+// https://astro.build/config
 export default defineConfig({
   site: 'https://chadcat7.github.io',
   base: '/',
-  integrations: [tailwind(), mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'css-variables'
-    },
+  markdown: {
     remarkPlugins: [remarkMdxCodeMeta, remarkMath],
     rehypePlugins: [rehypeKatex],
-    remarkRehype: {
-      footnoteLabel: 'Footnotes'
+  },
+  integrations: [astroExpressiveCode({
+    themes: ['vitesse-black'],
+    styleOverrides: {
+      codeFontFamily: "Iosevka Nerd Font",
+      codeFontSize: '18px',
+      uiFontFamily: "Product Sans",
+      uiFontSize: "17px",
+      codeLineHeight: '1.75',
     },
-    gfm: false
-  }), react()],
+    plugins: [
+      pluginFrames(), pluginTextMarkers()
+    ]
+  }), mdx(), react(), tailwind()],
   vite: {
     ssr: {
-      noExternal: ['mafs'],
-    },
-  },
+      noExternal: ['mafs']
+    }
+  }
 });
