d96768db916dd215e973cbdbf607adf46ca884a4
nam <namish@tuta.io>
2024-08-04 10:57:07 +0530

    made it look better on mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,39 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import rehypeKatex from 'rehype-katex';
-import remarkMath from 'remark-math';
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 import react from "@astrojs/react";
 import astroExpressiveCode from "astro-expressive-code";
-import { pluginFrames } from 'astro-expressive-code';
-import { pluginTextMarkers } from 'astro-expressive-code';
+import { pluginFrames } from "astro-expressive-code";
+import { pluginTextMarkers } from "astro-expressive-code";
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://namishh.github.io',
-  base: '/',
+  site: "https://namishh.github.io",
+  base: "/",
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
-  integrations: [astroExpressiveCode({
-    themes: ['vitesse-black'],
-    styleOverrides: {
-      codeFontFamily: "Iosevka Nerd Font",
-      codeFontSize: '18px',
-      uiFontFamily: "Product Sans",
-      uiFontSize: "17px",
-      codeLineHeight: '1.75',
-    },
-    plugins: [
-      pluginFrames(), pluginTextMarkers()
-    ]
-  }), mdx(), react(), tailwind()],
+  integrations: [
+    astroExpressiveCode({
+      themes: ["vitesse-black"],
+      styleOverrides: {
+        codeFontFamily: "Iosevka Nerd Font",
+        codeFontSize: "16px",
+        uiFontFamily: "Product Sans",
+        uiFontSize: "15px",
+        codeLineHeight: "1.75",
+      },
+      plugins: [pluginFrames(), pluginTextMarkers()],
+    }),
+    mdx(),
+    react(),
+    tailwind(),
+  ],
   vite: {
     ssr: {
-      noExternal: ['mafs']
-    }
-  }
+      noExternal: ["mafs"],
+    },
+  },
 });
